437b7a5e5cf6f38680e19e62181c473212ca3558
Roman Heviak <91373037+RomanHeviak@users.noreply.github.com>
2023-11-04 14:29:44 +0200

    update styles

--- home/src/components/Navbar/Navbar.tsx
+++ home/src/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ import './styles.css';
 const Navbar = () => {
     return (
         <div>
-            <Link className='link' to={'/'}> Home </Link>
-            <Link className='link' to={'/counter'}> Counter </Link>
+            <Link className='link' to={'/'}>Home</Link>
+            <Link className='link' to={'/counter'}>Counter</Link>
         </div>
     );
 };
