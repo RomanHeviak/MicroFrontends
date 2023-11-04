995cf73e7253bb70d49022c4dc525987758b8aed
Roman Heviak <102031342+Roman-Heviak@users.noreply.github.com>
2023-11-04 13:39:52 +0200

    auto

--- list/webpack.config.js
+++ list/webpack.config.js
@@ -6,7 +6,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: 'http://localhost:3002/'
+    publicPath: 'auto'
   },
 
   resolve: {
